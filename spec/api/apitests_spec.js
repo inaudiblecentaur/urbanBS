b39ce5db0f02f052ccb4b51cea0c793547ed9955
TheWhiteOx <thewhiteoxinbox@gmail.com>
2015-05-06 12:40:20 -0700

    add jasmine-node server test specfile

--- spec/api/apitests_spec.js
+++ spec/api/apitests_spec.js
@@ -1,3 +1,13 @@
 var request = require('request');
 
-it("Should")
+it("Should respond with 200 code to a GET request", function(done) {
+  request("http://localhost:3000", function(error, response, body) {
+    expect(body).toEqual("");
+    done();
+  });
+});
+
+it("Should response with an object to a POST request", function(done) {
+  request.post("http://localhost:3000").form({username: 'testuser', password: 'testpass'});
+  done();
+});
